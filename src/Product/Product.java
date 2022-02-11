6f1160ac0e575c193d7e1e69cc76c3f04186487a
Lee JaeGeun <ljg95924@github.com>
2022-02-11 22:55:36 +0900

    1차 완성

--- src/Product/Product.java
+++ src/Product/Product.java
@@ -4,14 +4,14 @@ public class Product {
     private long number;
     private String name;
     private int price;
-    private int stockNum;
+    private int stockAmount;
 
 
-    public Product(long number, String name, int price, int stockNum) {
+    public Product(long number, String name, int price, int stockAmount) {
         this.number = number;
         this.name = name;
         this.price = price;
-        this.stockNum = stockNum;
+        this.stockAmount = stockAmount;
     }
 
     public String getName() {
@@ -26,8 +26,21 @@ public class Product {
         return number;
     }
 
+    public void setStockAmount(int stockAmount) {
+        if (stockAmount <= 0) {
+            System.out.println("재고부족Exception! 해당 제품 현재 재고량은 " + this.stockAmount);
+        } else {
+            this.stockAmount = stockAmount;
+        }
+
+    }
+
+    public int getStockAmount() {
+        return stockAmount;
+    }
+
     @Override
     public String toString() {
-        return number + "   " + name + "    " + price + "   " + stockNum;
+        return number + "   " + name + "    " + price + "   " + stockAmount;
     }
 }
--- src/Product/ProductOrder.java
+++ src/Product/ProductOrder.java
@@ -1,9 +1,7 @@
 package Product;
 
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.text.DecimalFormat;
+import java.util.*;
 
 public class ProductOrder {
     public static void main(String[] args) {
@@ -19,7 +17,6 @@ public class ProductOrder {
             Scanner scanner = new Scanner(System.in);
             String UserInput = scanner.nextLine();
             productOrder.CompareInputValue(UserInput);
-
             productView.ListView(productList);
             productOrder.Order(productList);
         }
@@ -39,20 +36,64 @@ public class ProductOrder {
         }
     }
 
-    public List Order(List productList) {
+    public List Order(List<Product> productList) {
+        int totalPrice = 0;
+        List<Product> orderProduct = new ArrayList<>();
         do {
-            OrderOne()
+            System.out.print("상품번호 : ");
+            Scanner scanner = new Scanner(System.in);
+            String productCodeNum = scanner.nextLine();
+            System.out.print("수량: ");
+            String purchaseAmount = scanner.nextLine();
+
+            if (productCodeNum.isEmpty() && purchaseAmount.isEmpty()) {
+                break;
+            } else if (!productCodeNum.isEmpty() && !purchaseAmount.isEmpty()) {
+                Iterator<Product> iterator = productList.iterator();
+                int CodeNum = Integer.parseInt(productCodeNum);
+                int purchaseAmountNum = Integer.parseInt(purchaseAmount);
+                while (iterator.hasNext()) {
+                    Product product = iterator.next();
+                    if (CodeNum == product.getNumber()) {
+                        product.setStockAmount(product.getStockAmount() - purchaseAmountNum);
+                        if (CheckStock(product.getStockAmount(), purchaseAmountNum)) {
+                            totalPrice += (purchaseAmountNum * product.getPrice());
+                            orderProduct.add(new Product(product.getNumber(), product.getName(), product.getPrice(), purchaseAmountNum));
+                        }
+                        break;
+                    }
+
+                }
+            }
         }
-        while ()
-
+        while (true);
+        if (!orderProduct.isEmpty()) {
+            OrderList(orderProduct, totalPrice);
+        }
+        return productList;
     }
 
-    public List OrderOne(List productList) {
-        System.out.print("상품번호 : ");
-        Scanner scanner = new Scanner(System.in);
-        int productNum = scanner.nextInt();
-        System.out.print("수량: ");
-        int stockNum = scanner.nextInt();
 
+    public boolean CheckStock(int stockAmount, int purchaseAmount) {
+        if (stockAmount > purchaseAmount && stockAmount > 0) {
+            return true;
+        }
+        return false;
     }
+
+    public void OrderList(List<Product> orderProduct, int totalPrice) {
+        DecimalFormat formatter = new DecimalFormat("###,###,###");
+        System.out.println("주문내역: ");
+        for (Product product : orderProduct
+        ) {
+            System.out.println(product.getName() + " - " + product.getStockAmount() + "개");
+        }
+        System.out.println("주문금액: " + formatter.format(totalPrice) + "원");
+        if (totalPrice < 50000) {
+            totalPrice += 2500;
+            System.out.println("배송비: " + formatter.format(2500) + "원");
+        }
+        System.out.println("지불금액: " + formatter.format(totalPrice) + "원");
+    }
+
 }
